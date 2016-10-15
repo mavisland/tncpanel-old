ad82b41eccd9a59c6dfe888a0d373b5f714946b9
Tanju Yıldız <ben@tanjuyildiz.com>
2016-10-15 20:42:05 +0300

    creating header and sidebar styles.

--- js/tncpanel.js
+++ js/tncpanel.js
@@ -1,1 +1,11 @@
-console.log('init: tncpanel');
+(function ($) {
+  'use strict';
+
+  $('.app-aside .nav .has-child > a').on('click',function(e) {
+    e.preventDefault();
+    $(this).next('.nav-sub').slideToggle(300, function() {
+      $(this).parent().toggleClass('active');
+    });
+  });
+
+})(jQuery);
